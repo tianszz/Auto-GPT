b6201853fde50301f07cc700ca2581da4e55784e
tianszz <stianchst@gmail.com>
2023-04-21 22:38:34 +0800

    test template (#1)

---
+++ tests/test_template.py
@@ -0,0 +1,11 @@
+import unittest
+
+import tests.context
+from autogpt.token_counter import count_message_tokens, count_string_tokens
+
+
+class TestTokenCounter(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
